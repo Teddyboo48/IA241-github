b23e708c1245ddb2e91ff2e2d8bffb0d2d541cc0
EC2 Default User <ec2-user@ip-172-31-43-177.ec2.internal>
2023-01-26 21:09:08 +0000

    lec2

---
+++ Lab2.py
@@ -0,0 +1,20 @@
+'''
+Lab 2
+'''
+
+#6.1
+my_name = 'Tom'
+print(my_name.upper())
+my_id = 123
+print(my_id)
+#6.3
+
+my_id = your_id = 123
+print(your_id)
+my_id_str = '123'
+print(my_id_str)
+# print(my_name + my_id) NO
+print(my_name + my_id_str) #YES
+print(my_name * 3) #yes 
+print('hello world. This is my first python string'.split())
+# message='Tom's id is 123' No
---
+++ Lec2.py
@@ -0,0 +1,34 @@
+'''
+lec 2
+numbers, strings, and variables
+'''
+#print(  type('hello')   ) # signle line comment #String
+
+#print(  type(123)   ) #Integer
+
+#print(  type(123.123)   ) #Float
+
+#print('hello')
+#print("hello")
+#print("it's our 2nd python class")
+
+print('Hello World')
+print('Hello World'.lower()  )
+
+print(' hello world '.strip())
+
+print('hello world'.split()) #default, split by spaces
+print('hello world'.split("o")) #split by "o"
+
+print('hello ' + 'world')
+
+print (1+2)
+print(1-2)
+print(1*2)
+print(1/2)
+
+my_num = 123
+print(my_num+1)
+
+my_num = 234
+print(my_num)
